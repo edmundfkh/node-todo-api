5f51423345cd7320bcdc00f3a64e310f3af6ddf5
Edmund Fong <henchster@gmail.com>
2017-08-17 08:47:04 +0800

    add get /todo/:id

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./model/todo');
@@ -30,6 +31,24 @@ app.get('/todos', (req, res) => {
     });
 
 });
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send('LOL');
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
