89d205c14ae640b3b3e67a2de6d780ef56b74900
Edmund Fong <henchster@gmail.com>
2017-08-18 16:49:47 +0800

    add test cases for get /todos/:id

--- Server/server.js
+++ Server/server.js
@@ -41,7 +41,7 @@ app.get('/todos/:id', (req, res) => {
     }
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(404).send('LOL');
+            res.status(404).send();
         }
         res.send({todo});
     }).catch((e) => {
